73ffb14a58d73c581c05b78aca5388e06f937fa4
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-25 13:56:01 -0600

    Between with AND operator

--- SELECT_statement.sql
+++ SELECT_statement.sql
@@ -102,3 +102,23 @@ FROM
     employees
 WHERE
     first_name NOT LIKE ('%Jack%');
+
+/* BETWEEN operator always used by AND operator */
+/* Select all the information from the “salaries” table regarding contracts from 66,000 to 70,000 dollars per year. */
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    salary BETWEEN 66000 AND 70000;
+
+/* Retrieve a list with all individuals whose employee number is not between ‘10004’ and ‘10012’ */
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    emp_no NOT BETWEEN 10004 AND 10012;
+
+/* Select the names of all departments with numbers between ‘d003’ and ‘d006’ */
+select * from departments where dept_no between 'd003' AND 'd006';
