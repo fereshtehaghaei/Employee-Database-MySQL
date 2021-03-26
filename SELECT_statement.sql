81159caf715e04c70ae96eddb44628dea5f2d9e7
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-26 11:53:22 -0600

    HAVING and WHERE query

--- SELECT_statement.sql
+++ SELECT_statement.sql
@@ -331,8 +331,18 @@ WHERE conditions
 GROUP BY col_name(s)
 HAVING conditions
 ORDER BY col_name(s)
-
 /*===============*/
+
+/*Select the employee numbers of all individuals who have signed more than 1 contract after the 1st of January 2000 */
+SELECT 
+    emp_no
+FROM
+    dept_emp
+WHERE
+    from_date > '2000-01-01'
+GROUP BY emp_no
+HAVING COUNT(from_date) > 1
+ORDER BY emp_no;
 
 
 
