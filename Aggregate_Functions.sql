33d840eb1ca0d96f6fdc0776546f8baa85e64f52
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-28 17:01:25 -0600

    AVG function

--- Aggregate_Functions.sql
+++ Aggregate_Functions.sql
@@ -44,21 +44,11 @@ FROM
 /*============*/
 /* AVG */
 /*============*/
-
-
-
-/*============*/
-/* ROUND */
-/*============*/
-
-
-
-/*============*/
-/* COALESCE */
-/*============*/
-
-
-
-/*============*/
-/* IFNULL */
-/*============*/
+/*What is the average annual salary paid to employees who started after the 1st of January 1997? */
+SELECT 
+    AVG(salary)
+FROM
+    salaries
+WHERE
+    from_date > '1997-01-01';
+   
