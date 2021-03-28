89643f7b815bd8ebfdca2a928d033ae5b64e429f
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-28 17:27:32 -0600

    coalesce function

--- Aggregate_Functions.sql
+++ Aggregate_Functions.sql
@@ -84,3 +84,15 @@ ORDER BY dept_no ASC;
 /* IFNULL(expression 1, expression 2) 
 	works best with two arguments*/
 /*============*/
+
+/* Apply the IFNULL() function to the values from the first and second column, 
+so that ‘N/A’ is displayed whenever a department number has no value, 
+and ‘Department name not provided’ is shown if there is no value for ‘dept_name’.*/
+SELECT
+    IFNULL(dept_no, 'N/A') as dept_no,
+    IFNULL(dept_name,
+            'Department name not provided') AS dept_name,
+    COALESCE(dept_no, dept_name) AS dept_info
+FROM
+    departments_dup
+ORDER BY dept_no ASC;
