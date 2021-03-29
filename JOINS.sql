1075c0c89290125aca323e263641bf17a1ff00df
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-29 15:28:35 -0600

    left join example

--- JOINS.sql
+++ JOINS.sql
@@ -85,6 +85,19 @@ FROM
 /* LEFT JOINS */
 /*============*/
 
+/*Join the 'employees' and the 'dept_manager' tables to return a subset of all the employees whose last name is Markovitch. */
+select * from employees;
+select * from dept_manager;
+
+SELECT 
+    e.emp_no, e.first_name, e.last_name, m.dept_no, m.from_date
+FROM
+    employees e
+        LEFT JOIN
+    dept_manager m ON e.emp_no = m.emp_no
+WHERE
+    e.last_name = 'Markovitch'
+    ORDER BY m.dept_no DESC, e.emp_no;
 
 /*============*/
 /* RIGHT JOINS */
