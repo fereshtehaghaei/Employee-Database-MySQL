f147726237799569f27297ab707786da6ba75d20
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-29 12:20:44 -0600

    creating a duplicate table

--- JOINS.sql
+++ JOINS.sql
@@ -1,3 +1,21 @@
+DROP table departments_dup;
+CREATE TABLE departments_dup (
+    dept_no CHAR(4) NULL,
+    dept_name VARCHAR(40) NULL
+);
+
+INSERT INTO  departments_dup (dept_no, dept_name) SELECT * FROM departments;
+
+INSERT INTO departments_dup (dept_name)
+VALUES ('Public Relations');
+
+SELECT * from departments_dup;
+
+DELETE FROM departments_dup 
+WHERE
+    dept_no = 'd002'; 
+INSERT INTO departments_dup(dept_no) VALUES ('d010'), ('d011');
+
 /*============*/
 /* JOINS */
 /*============*/
