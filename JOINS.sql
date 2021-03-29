a322aca60cea13c3be5ec61e13797cd6ee711028
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-29 12:54:21 -0600

    inner joins

--- JOINS.sql
+++ JOINS.sql
@@ -53,6 +53,12 @@ WHERE
 
 /*============*/
 /* INNER JOINS */
+/* SELECT
+		table_1.column_name(s), table_2.column_name(s)
+	FROM
+		table_1
+	JOIN
+		table_2 ON table_1.column_name = table_2.column_name;*/
 /*============*/
 
 
