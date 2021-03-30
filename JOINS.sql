0dcce2e1e3b247489fa4eabe65b473e74390e083
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-30 11:29:13 -0600

    old join syntax (where)

--- JOINS.sql
+++ JOINS.sql
@@ -102,6 +102,17 @@ WHERE
 /*============*/
 /* old JOINS syntax */
 /*============*/
+/* Extract a list containing information about all managers’ employee number, 
+first and last name, department number, and hire date. 
+Use the old type of join syntax to obtain the result. */
+
+SELECT 
+    e.emp_no, e.first_name, e.last_name, m.dept_no, e.hire_date
+FROM
+    employees e,
+    dept_manager m
+WHERE
+    e.emp_no = m.emp_no;
 
 
 /*============*/
