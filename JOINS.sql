3d0bf38c8aad9f4b2abb2eff381a5ca852b80290
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-30 12:27:18 -0600

    more than one join

--- JOINS.sql
+++ JOINS.sql
@@ -155,16 +155,25 @@ select * from employees;
 select * from titles;
 select * from departments;
 
-select e.first_name, e.last_name, e.hire_date, t.title, m.from_date, d.dept_name
-from employees e
-join 
-dept_manager m on e.emp_no = m.emp_no
-join 
-departments d on m.dept_no = d.dept_no
-join
-titles t on e.emp_no = t.emp_no
-  where t.title = 'Manager'
-  ORDER BY e.emp_no; 
+SELECT 
+    e.first_name,
+    e.last_name,
+    e.hire_date,
+    t.title,
+    m.from_date,
+    d.dept_name
+FROM
+    employees e
+        JOIN
+    dept_manager m ON e.emp_no = m.emp_no
+        JOIN
+    departments d ON m.dept_no = d.dept_no
+        JOIN
+    titles t ON e.emp_no = t.emp_no
+WHERE
+    t.title = 'Manager'
+ORDER BY e.emp_no; 
+ 
 
 /*============*/
 /* UNION + UNION ALL */
