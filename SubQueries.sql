0cc0f0cea23b3cbaaee12a7cbe5e77731f0190bb
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-31 14:47:39 -0600

    Exists

--- SubQueries.sql
+++ SubQueries.sql
@@ -19,9 +19,22 @@ WHERE
             hire_date BETWEEN '1990-01-01' AND '1995-01-01');
 
 /*-------------------------*/
-/*
+/* EXISTS */
+/* checks row by row and returns Boolean value 
+tests row vaues for existence and it's faster*/
 /*-------------------------*/
-
+/* Select the entire information for all employees whose job title is “Assistant Engineer” */
+SELECT *
+FROM
+    employees e
+WHERE
+	EXISTS(SELECT 
+            *
+        FROM
+            titles t
+        WHERE
+        t.emp_no = e.emp_no
+         AND   t.title = 'Assistant Engineer');
 
 
 /*-------------------------*/
