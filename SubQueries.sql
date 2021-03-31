4f51cb56d10eacc4076d344980e09afb33302538
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-03-31 16:40:53 -0600

    subqueries

--- SubQueries.sql
+++ SubQueries.sql
@@ -42,22 +42,24 @@ WHERE
 and employee number 110039 as a manager to all employees from 10021 to 10040*/
 /* this will divided into two subsets Subset A and Subset B and */
 SELECT 
-    e.emp_no AS employee_id,
-    MIN(de.dept_no) AS department_code,
-    (SELECT 
-            emp_no
-        FROM
-            dept_manager
-        WHERE
-            emp_no = 110022) AS manager_id
+    A.*
 FROM
-    employees e
-        JOIN
-    dept_emp de ON e.emp_no = de.emp_no
-WHERE
-    e.emp_no <= 10020
-GROUP BY e.emp_no
-ORDER BY e.emp_no;
+    (SELECT 
+        e.emp_no AS employee_id,
+            MIN(de.dept_no) AS department_code,
+            (SELECT 
+                    emp_no
+                FROM
+                    dept_manager
+                WHERE
+                    emp_no = 110022) AS manager_id
+    FROM
+        employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
+    WHERE
+        e.emp_no <= 10020
+    GROUP BY e.emp_no
+    ORDER BY e.emp_no) AS A;
 
 
 /*-------------------------*/
