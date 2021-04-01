3ba66d75648a59fd8f894204bceb1a4d69d39512
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-04-01 09:45:12 -0600

    created a new table

--- SubQueries.sql
+++ SubQueries.sql
@@ -121,13 +121,24 @@ FROM
 
 
 /*-------------------------*/
+/* Starting your code with “DROP TABLE”, create a table called “emp_manager” 
+(emp_no – integer of 11, not null; dept_no – CHAR of 4, null; manager_no – integer of 11, not null). */
+
+drop table if exists emp_manager;
+
+CREATE TABLE emp_manager (
+    emp_no INT(11) NOT NULL,
+    dept_no CHAR(4) NULL,
+    manager_no INT(11) NOT NULL
+);
+SELECT 
+    *
+FROM
+    emp_manager;
 
 /*-------------------------*/
 
 
 
-/*-------------------------*/
-
-/*-------------------------*/
 
 
