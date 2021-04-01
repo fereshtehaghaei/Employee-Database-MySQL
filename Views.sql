5469fe4715f0786438653aae2d20cf8970f8b9af
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-04-01 11:21:21 -0600

    views

--- Views.sql
+++ Views.sql
@@ -3,9 +3,15 @@
 	doesn't contain any real data
     just shows data in the real/base table
     assign views name starting with v_ or w_
+    CREATE VIEW view_name AS
+	SELECT
+		col_1, col_2, ..., col_n
+	FROM	
+		table_name;
 */
 select * from dept_emp;
 
+/* retrieve employees that are registered more than once in dept_emp table */
 SELECT 
     emp_no, from_date, to_date, COUNT(emp_no) AS Num
 FROM
@@ -13,6 +19,7 @@ FROM
 GROUP BY emp_no
 HAVING Num > 1;
 
+/* Visualize only the period encompassing the last contract of each employee */
 CREATE OR REPLACE VIEW v_dept_emp_latest_date AS
     SELECT 
         emp_no, MAX(from_date) AS from_date, MAX(to_date) AS to_date
@@ -22,7 +29,7 @@ CREATE OR REPLACE VIEW v_dept_emp_latest_date AS
     
 SELECT * FROM employees.v_dept_emp_latest_date;
 
-
+/*  */
 
 
 
