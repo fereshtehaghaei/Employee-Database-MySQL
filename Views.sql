a401f4dbb9636e90a8af756c47d550f40ea64c49
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-04-01 11:13:49 -0600

    views

---
+++ Views.sql
@@ -0,0 +1,59 @@
+/*----- VIEWS -----*/
+/* a virtual table whose contents are obtained from an existing base tables 
+	doesn't contain any real data
+    just shows data in the real/base table
+    assign views name starting with v_ or w_
+*/
+select * from dept_emp;
+
+SELECT 
+    emp_no, from_date, to_date, COUNT(emp_no) AS Num
+FROM
+    dept_emp
+GROUP BY emp_no
+HAVING Num > 1;
+
+CREATE OR REPLACE VIEW v_dept_emp_latest_date AS
+    SELECT 
+        emp_no, MAX(from_date) AS from_date, MAX(to_date) AS to_date
+    FROM
+        dept_emp
+    GROUP BY emp_no;
+    
+SELECT * FROM employees.v_dept_emp_latest_date;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
