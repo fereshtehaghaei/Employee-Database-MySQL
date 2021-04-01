8acc1b020418aa9e97dca033c2b57a549bfe8548
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-04-01 14:39:11 -0600

    in and out stored procedures

--- Stored_Routines.sql
+++ Stored_Routines.sql
@@ -133,7 +133,20 @@ END$$
 DELIMITER ;
 
 
-/*
+/*Create a procedure called ‘emp_info’ that uses as parameters the first and the last name of an individual, and returns their employee number.*/
+DELIMITER $$
+CREATE PROCEDURE emp_info(in p_first_name varchar(255), in p_last_name varchar(255), out p_emp_no INTEGER)
+BEGIN
+SELECT 
+	e.emp_no
+INTO 
+	p_emp_no
+FROM 
+		employees e
+WHERE 
+		e.first_name = p_first_name AND e.last_name = p_last_name;
+END$$
+DELIMITER ;
 
 
 
