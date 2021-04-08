6305236592fe9901d71934c385122aa88e0db000
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-04-08 13:34:11 -0600

    SQL trigger exercise

--- Advanced_SQL_Topics.sql
+++ Advanced_SQL_Topics.sql
@@ -125,12 +125,18 @@ CREATE TRIGGER trig_hire_date
 BEFORE INSERT ON employees
 FOR EACH ROW
 BEGIN
-	IF NEW.hire_date < SYSDATE()THEN
-		SET NEW.hire_date = today;
+	IF NEW.hire_date > date_format(SYSDATE(), '%Y-%m-%d') THEN
+		SET NEW.hire_date = date_format(SYSDATE(), '%Y-%m-%d');
 	END IF;
 END $$
 DELIMITER ;
 
+INSERT employees VALUES ('999904', '1970-01-31', 'John', 'Johnson', 'M', '2025-01-01');  
+
+SELECT *  
+FROM  
+	employees
+ORDER BY emp_no DESC;
 
 
 
