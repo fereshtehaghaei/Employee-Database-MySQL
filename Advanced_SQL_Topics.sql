222d984e9bd26cb1f5f02db17549c23cec5fbc9d
Fereshteh Aghaei <fereshtehaghaei17@yahoo.com>
2021-04-09 10:37:18 -0600

    Case Statement exercise 1

--- Advanced_SQL_Topics.sql
+++ Advanced_SQL_Topics.sql
@@ -216,13 +216,13 @@ SELECT
 FROM 
 	table_name t
 JOIN 
-	if there any joins table_name tn ON t.col_1 = tn.col_2
+	if_there_any_joins table_name tn ON t.col_1 = tn.col_2
 WHERE
 	your condition;
 */
 
 /* ----------------------------------
-CASE statement with MANY CONDISTIONS
+CASE statement with Multiple WHEN
 -------------------------------------
 SELECT
 	column_name(s)
@@ -253,3 +253,23 @@ SELECT
 FROM 
 	table_name;
 */
+
+
+/*  
+obtain a result set containing the employee number, first name, and last name of all employees with a number higher than 109990. 
+Create a fourth column in the query, indicating whether this employee is also a manager, 
+according to the data provided in the dept_manager table, or a regular employee. 
+*/
+SELECT
+	e.emp_no,
+	e.first_name,
+    e.last_name,
+	CASE
+		WHEN dm.emp_no IS NOT NULL THEN 'Manager'
+		ELSE 'Employee'
+	END AS is_manager
+FROM
+	employee e
+JOIN
+	dept_manager dm ON e.emp_no = dm.emp_no
+WHERE emp_no > 109990;
